c248dbd8b20388e9cd5f933c8145661382a82d9d
fariasmi <mi.farias182@gmail.com>
2022-02-21 11:28:09 -0300

    adding logic and board game

--- app.js
+++ app.js
@@ -2,12 +2,25 @@
 const gamePlaces = document.querySelectorAll('.blocks');
 
 
+function gameBoard (playerObj){
+    let board = new Array(9);
+    board.splice(playerObj.position,0,playerObj.player);
+    console.log(board);
+}
+
+const player = (name,mark)=>{
+    return{name,mark}    
+}
 
 
-
-
-
-
+function restart (){
+    const plays = document.getElementsByClassName('playing');
+    while(plays[0]) {
+        plays[0].parentNode.removeChild(plays[0]);
+    }
+    
+        
+}
 
 function playingTurn(){
     gamePlaces.forEach(place =>place.addEventListener('click',()=>{
@@ -21,3 +34,5 @@ function playingTurn(){
 
 }
 playingTurn();
+document.getElementById('restartBtn'). addEventListener('click',restart)
+
