2174611d8557f473c6c5dbb9656f1603f2facfa4
fariasmi <mi.farias182@gmail.com>
2022-03-08 07:56:14 -0300

    adding winner condition

--- script.js
+++ script.js
@@ -1,59 +1,153 @@
 Player = (mark) => {
-    let turn = false;
-    const getMark = () => mark;
-    const setMark = () => mark;
-    const setTurn = () => turn;
-    return{getMark,setMark, setTurn}
+    let _turn = false;
+    
+    const setMark = (mark) =>{mark = mark}
+    const getMark = () => mark 
+    const getTurn = () => _turn;
+    const setTurn = (turn) =>{_turn = turn}
+    
+    return{getMark,setMark,getTurn,setTurn}
 }
 
-gameBoard = (()=>{
-    const gamePlaces = document.querySelectorAll('.block');
- 
-    let board = new Array(9);
-    function choseMarkPlayer(){
-   
+const gameBoard = (()=>{
+    let board = []
 
-    }
-            
-    function verifyPlayer(){
-          if(typeof playerX == 'undefined' && typeof playerO == 'undefined'){
-            return false;           
-        }else{
-            if(typeof playerO == 'undefined'){
-                                  
-        }
-                 
+    function markPosition(position,mark){
+       board[position] = mark;
+           
     }
 
-    function createPlayer(mark){
-        if(mark === 'X'){
-            const playerX = Player(mark);
+    function restartBoard (){
+        board.length = 0;
+    }
+
+    function checkWinner(mark){
+       const winComb = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+          ] 
+                
+
+        return winComb.some(combination => {
+            return combination.every(index => {
+                return board[index] === mark;
+            });
+          });    
+                
+        
+    }
+
+    function isDrawn(){
+        return board.length === 9 
+    }
+    return{
+        markPosition,
+        checkWinner,
+        restartBoard
+    }
+
+})();
+
+const players = (() =>{
+    let playerX = Player('X');
+    let playerO = Player('O');
+
+
+    function defineTurn(){
+        if(playerX.getTurn() === false && playerO.getTurn() === false){
             playerX.setTurn(true);
         }else{
-            const playerO= Player(mark);
-            playerO.setTurn(true);
+            if(playerX.getTurn() === true && playerO.getTurn() === false){
+                playerX.setTurn(false);
+                playerO.setTurn(true);
+            }else{
+                playerX.setTurn(true);
+                playerO.setTurn(false);
+               
+            }  
+
+          }
+          
+    }
+
+    function playerTurn(){
+        return playerX.getTurn() ? playerX : playerO;    
+    } 
+        
+    function deletePlayers(){
+        delete playerX;
+        delete playerO;
+    }
+
+    return {
+        defineTurn,
+        playerTurn,
+        deletePlayers
+    }
+
+})();
+
+const domView = (()=>{
+    const screen = document.getElementsByTagName('main');
+    const gamePlaces = document.querySelectorAll('[data-block]');
+    const restartBtn = document.getElementById('restartBtn');
+    
+ 
+    function startGame(){
+            players.defineTurn();
+            gamePlaces.forEach(place =>place.addEventListener('click',markBoard,{once:true}));
+    
+    }
+
+    function markBoard(e){
+        const player = players.playerTurn();
+        const place = e.target;
+        const playPosition = place.getAttribute('data-block');
+        place.textContent = player.getMark();
+        gameBoard.markPosition(playPosition,player.getMark());
+        players.defineTurn();
+        gameMessage(player.getMark());
+    }
+    
+    function gameMessage (player){
+        const message = document.createElement('p');
+        message.textContent = `It\'s' ${player} turn`
+        screen.appendChild(message);
+        if(gameBoard.checkWinner(player)){
+            message.textContent = `Winner's ${player}`
+            screen.appendChild(message);
         }
-
+        console.log('opora')
+    }
+    
+    
+    
+          
+    function restartGame(){
+        gamePlaces.forEach(place =>{
+            place.textContent= '';
+            place.removeEventListener('click', markBoard);
+        });
+         gameBoard.restartBoard();
+         players.deletePlayers();
+         startGame();
     }
 
-    function playing(){
-        gamePlaces.forEach(place =>place.addEventListener('click',createMark(place),{once:true}));
-        
-    }
-
-    function createMark(place){
-        
-        playing.classList.add('playing');
-        playing.textContent = player.getMark;
-        place.appendChild(playing);
-    }
-   
+    restartBtn.addEventListener('click',restartGame);
+    
     return{
-        playing
+        startGame
     }
+})();
 
-});
 
-const init = gameBoard();
 
-init.playing();
+domView.startGame();
+
+
